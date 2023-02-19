de05e06b345bda1f8147beeacd80f95c579507b3
TulioMeran <tulio.meran@nordstrom.com>
2023-02-19 15:54:30 -0400

    first commit

---
+++ src/test/java/com/tuliomeran/security/springsecurityexample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuliomeran.security.springsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
